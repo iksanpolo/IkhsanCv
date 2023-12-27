6b9402db6fb932b400eb76420c072acc92ad8b98
iksanpolo <63791358+iksanpolo@users.noreply.github.com>
2023-12-27 14:29:52 +0700

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,9 +20,9 @@ import { LinkedInIcon } from "../components/icons/LinkedInIcon";
 import { XIcon } from "../components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
+  name: "R IKHSAN NUR AKBAR",
+  initials: "R.I.N.A",
+  location: "Bogor, West Java, ID",
   locationLink: "https://www.google.com/maps/place/Wrocław",
   about:
     "Full Stack Engineer focused on building products with extra attention to details",
